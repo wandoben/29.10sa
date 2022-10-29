0ee27c12ec06ff5bb98db926b2f7c5f975b2234e
wandoben <wandobenplay@gmail.com>
2022-10-29 14:14:09 +0300

    a

--- q.py
+++ q.py
@@ -1,1 +1,9 @@
-import turtle
+spis_1 = [i for i in range (1,51)]
+spis_2=[i for i in range(50,0,-1)]
+spis_3=[i**2 for i in range(1,12)]
+spis_4=[i**2 for i in range(11,0,-1)]
+spis_5=[i for i in range(2,42,2)]
+spis_6=[i**2 if i%2==1 else i**2*-1 for i in range(1,11)]
+spis_7=[i]
+
+print(spis_6)
